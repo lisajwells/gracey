a4cceb7a4d6407b5939e01783eb834b1660dd8a4
Lisa Wells <lisawells999@gmail.com>
2017-04-16 13:01:35 -0400

    style subscribe-landing for mobile; functions edited by seo

--- wp-content/themes/gracey-holistic/functions.php
+++ wp-content/themes/gracey-holistic/functions.php
@@ -325,7 +325,7 @@ add_filter( 'genesis_footer_creds_text', 'ghh_footer_copyright_text' );
  * @link http://wpbeaches.com/changing-genesis-theme-copyright-line-footer-wordpress/
  */
 function ghh_footer_copyright_text () {
-	$copyright = '<div class="creditline"><p class="sitemap-link"><a href="'.get_bloginfo( 'url' ).'/sitemap">Sitemap</a></p><p>Copyright &copy; ' . date('Y') . ' &middot; <a href="'.get_bloginfo( 'url' ).'">Gracey Holistic Health</a> &middot; All&nbsp;Rights&nbsp;Reserved.</div>';
+	$copyright = '<div class="creditline"><p class="sitemap-link"><a href="'.get_bloginfo( 'url' ).'/sitemap">Sitemap A</a>/<a href="/page_tags-sitemap.xml">B</a> | Areas Served around <a href="/">Boston</a>, <a href="/acupuncture-massachusetts/">Massachusetts</a> | Offices in <a href="/acupuncture-brookline-ma/">Brookline</a> and <a href="/acupuncture-belmont-ma/">Belmont</a></p><p>Copyright &copy; ' . date('Y') . ' &middot; <a href="'.get_bloginfo( 'url' ).'">Gracey Holistic Health</a> &middot; All&nbsp;Rights&nbsp;Reserved.</div>';
 
 	return $copyright;
 }
